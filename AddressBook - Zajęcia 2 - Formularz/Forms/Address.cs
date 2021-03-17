c4cfcb007bd4f5bbc2900b373d4e4153f55c6b8c
Cezary Marczuk <77850@student.pb.edu.pl>
2021-03-17 20:10:51 +0100

    Dodaj pliki projektów.

---
+++ AddressBook - Zajęcia 2 - Formularz/Forms/Address.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook___Zajęcia_2___Formularz.Forms
+{
+    public class Address
+    {
+        [Display(Name = "Moja Ulica:")]
+        [StringLength(60, MinimumLength = 3), Required(ErrorMessage = "Podaj nazwe!")]
+        public string Street { get; set; }
+
+        [Display(Name = "Numer:")]
+        [Range(1,100,ErrorMessage ="Numer zbyt duży!")]
+        public string Number { get; set; }
+
+        [Display(Name = "Kod Pocztowy:")]
+        [RegularExpression(@"\d{2}-\d{3}", ErrorMessage ="Poprawnie podaj kod pocztowy!"),Required(ErrorMessage="Podaj kod pocztowy!")]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "Miasto:")]
+        public string City { get; set; }
+    }
+}
